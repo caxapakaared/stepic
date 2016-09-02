d3dcda433e32e1cd7df51c4935ec3074c01dccf9
caxapakaared <rebusiztysyachidetaley@bk.ru>
2016-09-02 13:14:43 +0000

    F

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 def app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    arr = []
-    for i in env:
-        arr += [i.encode]
-    return arr 
+    resp = env['QUERY_STRING'].split("&")
+    resp = [item+"\r\n" for item in resp]
+    return resp.encode 
