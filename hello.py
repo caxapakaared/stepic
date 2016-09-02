93b300b4dfe36a4726d8109f071f5f038c27b0e1
caxapakaared <rebusiztysyachidetaley@bk.ru>
2016-09-02 12:59:09 +0000

    bjk

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 def app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return  [env]
+    arr = []
+    for i in env:
+        arr += [i.encode]
+    return arr 
