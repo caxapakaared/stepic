74d2483e7677e0a1785d74a0ddf7a5521fda2a13
Caxap <rebusiztysyachidetaley@bk.ru>
2016-09-27 22:28:14 +0400

    ddd

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+!oar3v#50^0i9e5qe@dymszls$hr8%3#)1t!x%rsj5ig3o!*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ git config --global user.email "rebusiztysyachidetaley@bk.ru"
 git config --global user.name "caxapakaared" 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:app &
+source web/myvenv/bin/activate
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
