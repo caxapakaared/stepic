96a871286f71be5d10a3202f67f00cbbdcddf16c
caxapakaared <rebusiztysyachidetaley@bk.ru>
2016-11-13 23:18:45 +0000

    base start

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 git config --global user.email "rebusiztysyachidetaley@bk.ru"
 git config --global user.name "caxapakaared" 
+sudo /etc/init.d/mysql start
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-source web/myvenv/bin/activate
 gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
 gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
