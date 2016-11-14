d0cbc1a25c0e1d3eb45cd38bf68849c75d792d36
caxapakaared <rebusiztysyachidetaley@bk.ru>
2016-11-14 04:32:54 +0000

    database2

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class Question(models.Model):
+    title
+    text
+    added_ad
+    rating
+    author
+    likes
+
+class Answer
+    text
+    added_at
+    question
+    author
+
+
+
+
+
+
+
+
 # Create your models here.
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 git config --global user.email "rebusiztysyachidetaley@bk.ru"
 git config --global user.name "caxapakaared" 
 sudo /etc/init.d/mysql start
-
+mysql -uroot -e "create database ask"
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
