a6cf0701648b048e3582322358f26de473431eb4
caxapakaared <rebusiztysyachidetaley@bk.ru>
2016-11-15 03:25:46 +0000

    models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
-    title
-    text
-    added_ad
-    rating
-    author
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    added_ad = models.DateTimeField(default=timezone.now)
+    rating = 0
+    author = models.ForeignKey('auth.User')
     likes
 
 class Answer
-    text
-    added_at
-    question
+    text = models.TextField()
+    added_at = models.DateTimeField(default=timezone.now)
+    question 
+    author = models.ForeignKey('auth.User')
     author
 
 
