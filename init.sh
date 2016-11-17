37ab578e3a2eb3ae8eb74bc4c2ec960861a96927
Caxap <rebusiztysyachidetaley@bk.ru>
2016-11-17 05:47:56 +0400

    python2

--- init.sh
+++ init.sh
@@ -2,9 +2,8 @@ git config --global user.email "rebusiztysyachidetaley@bk.ru"
 git config --global user.name "caxapakaared" 
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database ask"
-pip3 install pymysql
-python /home/box/web/ask/manage.py makemigrations
-python /home/box/web/ask/manage.py migrate
+sudo pip install pymysql
+python /home/box/web/ask/manage.py syncdb
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
